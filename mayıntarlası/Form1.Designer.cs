ab77c9a54946ed1253d26ffb8a7a80472b8f1cae
eyup218 <eyup218@RPC07>
2024-03-18 12:44:53 +0300

    Proje dosyası ekle.

---
+++ mayıntarlası/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace mayıntarlası
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.p00 = new System.Windows.Forms.PictureBox();
+            this.txtSatir = new System.Windows.Forms.TextBox();
+            this.txtSutun = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.p01 = new System.Windows.Forms.PictureBox();
+            this.p02 = new System.Windows.Forms.PictureBox();
+            this.p03 = new System.Windows.Forms.PictureBox();
+            this.p10 = new System.Windows.Forms.PictureBox();
+            this.p11 = new System.Windows.Forms.PictureBox();
+            this.p12 = new System.Windows.Forms.PictureBox();
+            this.p13 = new System.Windows.Forms.PictureBox();
+            this.p20 = new System.Windows.Forms.PictureBox();
+            this.p21 = new System.Windows.Forms.PictureBox();
+            this.p22 = new System.Windows.Forms.PictureBox();
+            this.p23 = new System.Windows.Forms.PictureBox();
+            this.p30 = new System.Windows.Forms.PictureBox();
+            this.p31 = new System.Windows.Forms.PictureBox();
+            this.p32 = new System.Windows.Forms.PictureBox();
+            this.p33 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.p00)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p03)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p23)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p30)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p31)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p32)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p33)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Location = new System.Drawing.Point(265, 47);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(75, 23);
+            this.btnGonder.TabIndex = 0;
+            this.btnGonder.Text = "Gönder";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.btnGonder_Click);
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.Location = new System.Drawing.Point(265, 9);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(75, 23);
+            this.btnYeni.TabIndex = 1;
+            this.btnYeni.Text = "Yeni Oyun";
+            this.btnYeni.UseVisualStyleBackColor = true;
+            this.btnYeni.Click += new System.EventHandler(this.btnYeni_Click);
+            // 
+            // p00
+            // 
+            this.p00.BackColor = System.Drawing.SystemColors.Control;
+            this.p00.Location = new System.Drawing.Point(27, 93);
+            this.p00.Name = "p00";
+            this.p00.Size = new System.Drawing.Size(75, 64);
+            this.p00.TabIndex = 2;
+            this.p00.TabStop = false;
+            // 
+            // txtSatir
+            // 
+            this.txtSatir.Location = new System.Drawing.Point(177, 12);
+            this.txtSatir.Name = "txtSatir";
+            this.txtSatir.Size = new System.Drawing.Size(33, 20);
+            this.txtSatir.TabIndex = 3;
+            // 
+            // txtSutun
+            // 
+            this.txtSutun.Location = new System.Drawing.Point(177, 49);
+            this.txtSutun.Name = "txtSutun";
+            this.txtSutun.Size = new System.Drawing.Size(33, 20);
+            this.txtSutun.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(109, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "satır";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(109, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "sütun";
+            // 
+            // p01
+            // 
+            this.p01.BackColor = System.Drawing.SystemColors.Control;
+            this.p01.Location = new System.Drawing.Point(135, 93);
+            this.p01.Name = "p01";
+            this.p01.Size = new System.Drawing.Size(75, 64);
+            this.p01.TabIndex = 7;
+            this.p01.TabStop = false;
+            // 
+            // p02
+            // 
+            this.p02.BackColor = System.Drawing.SystemColors.Control;
+            this.p02.Location = new System.Drawing.Point(245, 92);
+            this.p02.Name = "p02";
+            this.p02.Size = new System.Drawing.Size(75, 64);
+            this.p02.TabIndex = 8;
+            this.p02.TabStop = false;
+            // 
+            // p03
+            // 
+            this.p03.BackColor = System.Drawing.SystemColors.Control;
+            this.p03.Location = new System.Drawing.Point(351, 92);
+            this.p03.Name = "p03";
+            this.p03.Size = new System.Drawing.Size(75, 64);
+            this.p03.TabIndex = 9;
+            this.p03.TabStop = false;
+            // 
+            // p10
+            // 
+            this.p10.BackColor = System.Drawing.SystemColors.Control;
+            this.p10.Location = new System.Drawing.Point(27, 182);
+            this.p10.Name = "p10";
+            this.p10.Size = new System.Drawing.Size(75, 64);
+            this.p10.TabIndex = 10;
+            this.p10.TabStop = false;
+            // 
+            // p11
+            // 
+            this.p11.BackColor = System.Drawing.SystemColors.Control;
+            this.p11.Location = new System.Drawing.Point(135, 182);
+            this.p11.Name = "p11";
+            this.p11.Size = new System.Drawing.Size(75, 64);
+            this.p11.TabIndex = 11;
+            this.p11.TabStop = false;
+            // 
+            // p12
+            // 
+            this.p12.BackColor = System.Drawing.SystemColors.Control;
+            this.p12.Location = new System.Drawing.Point(245, 181);
+            this.p12.Name = "p12";
+            this.p12.Size = new System.Drawing.Size(75, 64);
+            this.p12.TabIndex = 12;
+            this.p12.TabStop = false;
+            // 
+            // p13
+            // 
+            this.p13.BackColor = System.Drawing.SystemColors.Control;
+            this.p13.Location = new System.Drawing.Point(351, 181);
+            this.p13.Name = "p13";
+            this.p13.Size = new System.Drawing.Size(75, 64);
+            this.p13.TabIndex = 13;
+            this.p13.TabStop = false;
+            // 
+            // p20
+            // 
+            this.p20.BackColor = System.Drawing.SystemColors.Control;
+            this.p20.Location = new System.Drawing.Point(27, 271);
+            this.p20.Name = "p20";
+            this.p20.Size = new System.Drawing.Size(75, 64);
+            this.p20.TabIndex = 14;
+            this.p20.TabStop = false;
+            // 
+            // p21
+            // 
+            this.p21.BackColor = System.Drawing.SystemColors.Control;
+            this.p21.Location = new System.Drawing.Point(135, 271);
+            this.p21.Name = "p21";
+            this.p21.Size = new System.Drawing.Size(75, 64);
+            this.p21.TabIndex = 15;
+            this.p21.TabStop = false;
+            // 
+            // p22
+            // 
+            this.p22.BackColor = System.Drawing.SystemColors.Control;
+            this.p22.Location = new System.Drawing.Point(245, 270);
+            this.p22.Name = "p22";
+            this.p22.Size = new System.Drawing.Size(75, 64);
+            this.p22.TabIndex = 16;
+            this.p22.TabStop = false;
+            // 
+            // p23
+            // 
+            this.p23.BackColor = System.Drawing.SystemColors.Control;
+            this.p23.Location = new System.Drawing.Point(351, 270);
+            this.p23.Name = "p23";
+            this.p23.Size = new System.Drawing.Size(75, 64);
+            this.p23.TabIndex = 17;
+            this.p23.TabStop = false;
+            // 
+            // p30
+            // 
+            this.p30.BackColor = System.Drawing.SystemColors.Control;
+            this.p30.Location = new System.Drawing.Point(27, 352);
+            this.p30.Name = "p30";
+            this.p30.Size = new System.Drawing.Size(75, 64);
+            this.p30.TabIndex = 18;
+            this.p30.TabStop = false;
+            // 
+            // p31
+            // 
+            this.p31.BackColor = System.Drawing.SystemColors.Control;
+            this.p31.Location = new System.Drawing.Point(135, 352);
+            this.p31.Name = "p31";
+            this.p31.Size = new System.Drawing.Size(75, 64);
+            this.p31.TabIndex = 19;
+            this.p31.TabStop = false;
+            // 
+            // p32
+            // 
+            this.p32.BackColor = System.Drawing.SystemColors.Control;
+            this.p32.Location = new System.Drawing.Point(245, 351);
+            this.p32.Name = "p32";
+            this.p32.Size = new System.Drawing.Size(75, 64);
+            this.p32.TabIndex = 20;
+            this.p32.TabStop = false;
+            // 
+            // p33
+            // 
+            this.p33.BackColor = System.Drawing.SystemColors.Control;
+            this.p33.Location = new System.Drawing.Point(351, 351);
+            this.p33.Name = "p33";
+            this.p33.Size = new System.Drawing.Size(75, 64);
+            this.p33.TabIndex = 21;
+            this.p33.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.p33);
+            this.Controls.Add(this.p32);
+            this.Controls.Add(this.p31);
+            this.Controls.Add(this.p30);
+            this.Controls.Add(this.p23);
+            this.Controls.Add(this.p22);
+            this.Controls.Add(this.p21);
+            this.Controls.Add(this.p20);
+            this.Controls.Add(this.p13);
+            this.Controls.Add(this.p12);
+            this.Controls.Add(this.p11);
+            this.Controls.Add(this.p10);
+            this.Controls.Add(this.p03);
+            this.Controls.Add(this.p02);
+            this.Controls.Add(this.p01);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSutun);
+            this.Controls.Add(this.txtSatir);
+            this.Controls.Add(this.p00);
+            this.Controls.Add(this.btnYeni);
+            this.Controls.Add(this.btnGonder);
+            this.Name = "Form1";
+            this.Text = "Bom";
+            ((System.ComponentModel.ISupportInitialize)(this.p00)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p03)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p23)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p30)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p31)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p32)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p33)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.Button btnYeni;
+        private System.Windows.Forms.PictureBox p00;
+        private System.Windows.Forms.TextBox txtSatir;
+        private System.Windows.Forms.TextBox txtSutun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox p01;
+        private System.Windows.Forms.PictureBox p02;
+        private System.Windows.Forms.PictureBox p03;
+        private System.Windows.Forms.PictureBox p10;
+        private System.Windows.Forms.PictureBox p11;
+        private System.Windows.Forms.PictureBox p12;
+        private System.Windows.Forms.PictureBox p13;
+        private System.Windows.Forms.PictureBox p20;
+        private System.Windows.Forms.PictureBox p21;
+        private System.Windows.Forms.PictureBox p22;
+        private System.Windows.Forms.PictureBox p23;
+        private System.Windows.Forms.PictureBox p30;
+        private System.Windows.Forms.PictureBox p31;
+        private System.Windows.Forms.PictureBox p32;
+        private System.Windows.Forms.PictureBox p33;
+    }
+}
+
---
+++ mayıntarlası/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mayıntarlası
+{
+    public partial class Form1 : Form
+    {
+        byte[,] dizi = new byte[4, 4];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGonder_Click(object sender, EventArgs e)
+        {
+            byte satir = byte.Parse(txtSatir.Text);
+            byte sutun = byte.Parse(txtSutun.Text);
+            PictureBox kutu = this.Controls.Find("p" + satir + sutun, true)[0] as PictureBox;
+            byte durum = dizi[satir, sutun];
+            if (durum == 0)
+            {
+                kutu.BackColor = Color.Green;
+            }
+            else
+            {
+                kutu.BackColor = Color.Red;
+            }
+        }
+
+        private void btnYeni_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            int satirRastgele = rastgele.Next(4);
+            int sutunRastgele = rastgele.Next(4);
+            dizi[satirRastgele, sutunRastgele] = 1;
+            for (byte satir = 0; satir < 4; satir++)
+            {
+                for (byte sutun = 0; sutun < 4; sutun++)
+                {
+                    PictureBox kutu = this.Controls.Find("p" + satir + sutun, true)[0] as PictureBox;
+                    kutu.BackColor = SystemColors.Control;
+                }
+            }
+        }
+    }
+}
